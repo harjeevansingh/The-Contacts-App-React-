e5a33a52c3ed564a6b70bbe93330c654a959f3ea
harjeevansingh <harjeevansingh01@gmail.com>
2021-12-05 17:10:38 +0530

    Updated ReadMe

--- ContactsUI/ContactsUI/src/index.js
+++ ContactsUI/ContactsUI/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import middlewares from './middlewares';
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducers, middlewares);
 
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        
+    </Provider>
+    ,document.getElementById('root'));
